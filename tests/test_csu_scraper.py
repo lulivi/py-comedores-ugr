e8ff58dd5da3aa153da165556e2066ee27b5d9a8
lulivi <luislivilla@gmail.com>
2017-11-13 22:14:31 +0100

    Added test module for scu_scrapper

---
+++ tests/test_csu_scraper.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Test CsuScraper class.
+
+Copyright 2017, Luis Liñán (luislivilla@gmail.com)
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+"""
+
+# External imports
+import unittest
+import json
+
+# Local imports
+import __init__ as init
+from scu_scraper import ScuScraper
+
+assert init
+
+# === Clase test ===
+class TestSciScrapper(unittest.TestCase):
+    """Comedores ugr api test class."""
+
+    def tes_get_week_menu(self):
+        pass
+
+    def tes_save_week_menu(self):
+        pass
+
+    def tes_update_week_menu(self):
+        pass
+
+
+def setUpModule():
+    """Set up Module method."""
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
